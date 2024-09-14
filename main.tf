f88b5e6b20310504e793d311c9bd164199fb1c8b
gyanm <kharelgyanoo@gmail.com>
2024-09-14 00:48:56 -0400

    tf vars

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
+  tenant_id       = var.AZURE_TENANT_ID
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
 }
 
 resource "azurerm_resource_group" "aks_rg" {
