605f8fa1fe80fd0dbcbfca09a14b3cd769117d6c
gyanm <kharelgyanoo@gmail.com>
2024-09-15 01:57:39 -0400

    api added

--- api/main.py
+++ api/main.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+todos = [{"title": "make coffee", "status": "pending"}, {"title": "Complete assignment", "status": "done"}]
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/todos")
+def get_todos():
+    return todos
