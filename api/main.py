0dc103d38188b4675da2b4ed87afa046dea8187a
gyanm <kharelgyanoo@gmail.com>
2024-09-15 02:28:59 -0400

    added api

--- api/main.py
+++ api/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+
+class TodoModel(BaseModel):
+    title: str
+    status: str
 
 todos = [{"title": "make coffee", "status": "pending"}, {"title": "Complete assignment", "status": "done"}]
 
@@ -15,3 +21,12 @@ def read_root():
 @app.get("/todos")
 def get_todos():
     return todos
+
+@app.post("/todos")
+def create_todo(todo: TodoModel):
+    todos.append(todo)
+
+    return JSONResponse(
+        content= {'message': 'Created'},
+        status_code=201
+    )
