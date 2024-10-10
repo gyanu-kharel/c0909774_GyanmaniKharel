3e96a2842ed55fa17d8a753dd752c98153369d3f
gyanm <kharelgyanoo@gmail.com>
2024-10-10 10:12:08 -0400

    changes

--- terraform/elk.tf
+++ terraform/elk.tf
@@ -1,3 +1,13 @@
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+
 resource "kubernetes_namespace" "logging" {
   metadata {
     name = "logging"
